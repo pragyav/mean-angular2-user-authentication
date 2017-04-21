2202ed5c3a02a96728dd72fc66dc41fd501a5068
anshi <anshi>
2017-04-21 12:44:17 +0300

    First Commit

---
+++ client/app/app.config.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var AppConfig = (function () {
+    function AppConfig() {
+        this.apiUrl = 'http://localhost:4000';
+    }
+    return AppConfig;
+}());
+exports.AppConfig = AppConfig;
+;
+//# sourceMappingURL=app.config.js.map
